febd129208c9fc87438076a4c2ed0887ad398c81
CsRecords Deploy <deploy@csrecords.com>
2026-09-11 15:36:20 -0600

    Fix Cloud Run HTTPS: trustProxies * for API

--- BackEnd-Api/core-api/bootstrap/app.php
+++ BackEnd-Api/core-api/bootstrap/app.php
@@ -12,6 +12,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->trustProxies(at: '*');
         $middleware->alias([
             'dependencia' => \App\Http\Middleware\ResolveDependencia::class,
             'licencia' => \App\Http\Middleware\CheckLicencia::class,
